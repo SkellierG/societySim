a7d34ec8781fbdbfe2614224239c6ddb15f1c0e0
SkellierG <benja314gonzalez@gmail.com>
2024-02-25 18:41:50 -0300

    update

---
+++ script.js
@@ -0,0 +1,16 @@
+let countries = [];
+
+function makeCountries() {
+  for (let i = 0; i < countriesJson.length; i++) {
+    countries[tagJson] = {
+      tag: tagJson,
+      culture: "",
+      s_culture: [],
+      religion: "",
+      s_religion: [],
+      tresoury: 0,
+      prestige: 0
+    }
+  }
+}
+
